cdf4e646c178f9e1a314487b1da412843516d84e
agnnn <rjpradeep75@gmail.com>
2018-04-27 13:57:47 -0400

    add backends

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "tillgangstorage"  ## Storage account name of remote state
     container_name = "tillgang-remote-state-container" ## Container name of the remote state
-    key = "fufunctionsDeployment.terraform.tfstate"     ## Change this to whatever resource you are provisioning eg. key = "baas.terraform.tfstate" or key = "DLTcosmosDB.terraform.tfstate"
+    key = "tfDeployment.terraform.tfstate"     ## Change this to whatever resource you are provisioning eg. key = "baas.terraform.tfstate" or key = "DLTcosmosDB.terraform.tfstate"
     resource_group_name = "tillgang-remote-state-terraform" ## Resource Group of the backend storage account
   }
 }
